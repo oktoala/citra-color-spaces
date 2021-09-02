ba02a52a7f2879cb5e89e7165604ed0b928af4a6
oktoala <okta.yoga.syah@gmail.com>
2021-09-02 12:33:39 +0800

    Create RGB slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import Image from './img/unnamed.jpg';
+import { Container, Slider, Grid, Typography } from '@material-ui/core';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <SliderRGB />
+    </main>
   );
 }
+
+const SliderRGB = () => {
+
+  const handleRed = (event, newValue) => {
+    setRed(newValue);
+  }
+  const handleGreen = (event, newValue) => {
+    setGreen(newValue);
+  }
+  const handleBlue = (event, newValue) => {
+    setBlue(newValue);
+  }
+
+  const [red, setRed] = useState(50);
+  const [green, setGreen] = useState(50);
+  const [blue, setBlue] = useState(50);
+
+  return (
+    <Container maxWidth="sm">
+      <MySlider color="red"/>
+      <MySlider color="green"/>
+      <MySlider color="blue"/>
+    </Container>
+  )
+}
+
+const MySlider = (props) => {
+  return (
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <PrettoSlider color={props.color} max="255" valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
+        </Grid>
+        <Grid item >
+          <Typography>Hah</Typography>
+        </Grid>
+      </Grid>
+
+  );
+}
+
+
+const PrettoSlider = withStyles({
+  root: props => ({
+    color: props.color,
+    height: 8,
+  }),
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
 
 export default App;
